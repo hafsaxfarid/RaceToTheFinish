6f7f8482e731320239851041fa94e18d3e889862
hafsaxfarid <59932046+hafsaxfarid@users.noreply.github.com>
2021-04-22 23:04:24 -0400

    Initial/Additions
    First Commit and some layout. Camera follows player.

---
+++ GAME3002_A3/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private float smoothSpeed = 0.125f;
+
+    [SerializeField]
+    private Vector3 camOffset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPos = player.position + camOffset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothedPos;
+
+        //transform.LookAt(player);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# RaceToTheFinish
- GAME3002_A3 - Final assignment for the course. Creating a simple 3D race to the finihs platformer game.
+GAME3002_A3 - Final Assignment for the course.
+
+Creating a simple 3D race to the finish platformer game.
+
+Try and reach the finish line before time is up.
+3 enviroment zones that can be played when the key to their door is found.
+Hazards that are deadly on touch and some zones that throw you for a loop on speed control.
